b92ad5d437366597e201046261eb1b80f34c788b
Nathan Merritt <gnm@the-merritts.net>
2013-09-09 19:01:10 -0400

    added /delay endpoint to time loads

--- splitter.py
+++ splitter.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def splitter():
     return render_template("splitter.html")
+
+@app.route("/delay/<int:ms>")
+def delay(ms):
+    time.sleep(0.001*ms)
+    return "This page loaded in {}ms".format(ms)
 
 if __name__ == "__main__":
     app.run(debug=True)
